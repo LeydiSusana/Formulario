6631259770665453721b0b9b54bd1de293d461c9
LeydiSusana <a352110283@alumno.uttehuacan.edu.mx>
2022-10-17 22:34:30 -0500

    Formulario

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mt-3">
+    <Header />
+    <Formulario />
+  </div>
+</template>
+
+<script>
+  
+  import Header from "./components/Header.vue"
+  import Formulario from "./components/Formulario.vue"
+  export default {
+    components: {
+      Formulario,
+      Header,
+    }
+  }
+</script>
+
+<style scoped>
+  body {
+    margin: 0px 2rem;
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container mt-3">
+        <div class="row">
+          <div class="col-12 col-md-6">
+            <h1>Formularios</h1>
+          </div>
+          <div class="col-12 col-md-6">
+            <img :src="url" :alt="alt" width="100">
+          </div>
+       </div>
+
+       
+        <hr />
+    </div>
+</template>
+
+<script>
+export default{
+      data: () => ({
+      url: "https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg",
+      alt: "vue.js",
+    
+    }),
+  };
+    
+  </script>
